e3636995cada22fa830ccf7a6de274c6cbbf2b84
liusy182 <liusy182@gmail.com>
2016-09-06 17:21:51 +0800

    second workflow

--- MeetYourFace/InstantScanViewController.swift
+++ MeetYourFace/InstantScanViewController.swift
@@ -8,28 +8,77 @@
 
 import UIKit
 
-class InstantScanViewController: UIViewController {
+import CoreImage
+import AVFoundation
+import Cartography
 
+
+class InstantScanViewController: UIViewController {
+    
+    var label = UILabel()
+    var button = UIButton()
+    var isFirstLoad = true;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setup()
         // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewDidAppear(animated: Bool) {
+        
+        if(isFirstLoad) {
+            isFirstLoad = false
+            let vc = ScanViewController()
+            presentViewController(vc, animated: true, completion: nil)
+        }
+        super.viewDidAppear(animated)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    private func setup() {
+        label.text = "Raffles Place is available! Do you want to have it booked for the next hour?"
+        
+        label.frame = CGRectMake(0, 0, 300, 50)
+        label.textAlignment = NSTextAlignment.Center
+        label.center = CGPoint(x: view.center.x,
+                                y: view.center.y / 1.5)
+        label.textColor = UIColor.blackColor()
+        label.numberOfLines = 0
+        
+        view.addSubview(label)
+        
+        button.setTitle("Book", forState: .Normal)
+        button.setTitleColor(.whiteColor(), forState: .Normal)
+        button.frame = CGRect(
+            origin: CGPoint(x: 0, y: 0),
+            size: CGSize(width: 200, height: 50))
+        
+        button.center = CGPoint(x: view.center.x,
+                                y: view.center.y * 4 / 3)
+        button.backgroundColor = UIColor.blackColor()
+        
+        button.addTarget(
+            self,
+            action: #selector(InstantScanViewController.meetingConfirmed(_:)),
+            forControlEvents: UIControlEvents.TouchUpInside)
+        view.addSubview(button)
     }
-    */
-
+    
+    func meetingConfirmed(sender: UIButton) {
+        let alertController = UIAlertController(title: "Booking Successful", message: "You have sucessfully booked Raffles Place for 1 hour", preferredStyle: .Alert)
+        
+        // Create the actions
+        let okAction = UIAlertAction(title: "OK",
+                                     style: UIAlertActionStyle.Default,
+                                     handler: nil)
+        
+        // Add the actions
+        alertController.addAction(okAction)
+        
+        // Present the controller
+        self.presentViewController(alertController, animated: true, completion: nil)
+    }
+    
+    
 }
+
